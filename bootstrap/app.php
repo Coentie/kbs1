b81f9456fb02ad85fe510feb7bf472460ada276c
Coen Nijenhuis <nijenhuiscoen@gmail.com>
2018-06-10 18:04:53 +0200

    $m

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,21 @@
 <?php
 
 /**
- * Bootstraps the applications.
+ * Bootstrapping the applications.
  *
  * Starting the session.
  * Requiring in the autoloader from Composer.
+ * Load the env variables through vlucas/phpdontenv.
  */
 session_start();
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+try {
+    (new \Dotenv\Dotenv(__DIR__ . '/..//'))->load();
+}catch (\Dotenv\Exception\InvalidPathException $e) {
+    // Do nothing
+}
+
+var_dump(getenv('APP_NAME'));
+
